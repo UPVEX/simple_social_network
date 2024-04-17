734047815a7350645da9bae2b180bdb138a135b2
unknown <nimamollayi@gmail.com>
2024-04-17 23:28:05 +0330

    creating account app

---
+++ account/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2 = forms.CharField(label= 'Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError('this email is already used !!! ')
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username=username).exists()
+        if user:
+            raise ValidationError('username already exist . ')
+        return username
+
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get('password1')
+        p2 = cd.get('password2')
+
+        if p1 and p2 and p1 != p2:
+            raise ValidationError('passwords must match')
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
---
+++ account/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from .forms import UserRegistrationForm, UserLoginForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class UserRegisterView(View):
+    form_class = UserRegistrationForm
+    template_name = 'account/register.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+
+
+    def get(self, request):
+        form = self.form_class() # using this instead of --> form = UserRegistrationForm()
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)  # using this instead of --> form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password1'])
+            messages.success(request, 'User Created Successfully', 'success')
+            return redirect('home:home')
+        return render(request, self.template_name, {'form':form})
+
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'account/login.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form':form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'user loged in succesfully', 'success')
+                return redirect('home:home')
+            messages.error(request, 'username or password is wrong', 'warning')
+
+        return render(request, self.template_name, {'form':form})
+
+
+class UserLogoutView(LoginRequiredMixin, View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, 'Logout successfully', 'success')
+        return redirect('home:home')
