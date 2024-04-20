befe5dab74e881d780d0661d5e52ba6b6e278b95
unknown <nimamollayi@gmail.com>
2024-04-20 11:38:35 +0330

    add profile and post option

--- account/views.py
+++ account/views.py
@@ -62,3 +62,9 @@ class UserLogoutView(LoginRequiredMixin, View):
         logout(request)
         messages.success(request, 'Logout successfully', 'success')
         return redirect('home:home')
+
+
+class UserProfileView(LoginRequiredMixin, View):
+    def get(self, request, user_id):
+        user = User.objects.get(id=user_id)
+        return render(request, 'account/profile.html', {'user':user})
