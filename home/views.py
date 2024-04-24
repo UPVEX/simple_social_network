42da9b8aa08a17beac49f55f90ba7c8804a6a8fa
unknown <nimamollayi@gmail.com>
2024-04-24 11:37:11 +0330

    add update option to post detail

--- home/views.py
+++ home/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .models import Post
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import PostUpdateForm
 
 
 class HomeView(View):
@@ -15,7 +17,7 @@ class PostDetailView(View):
         return render(request, 'home/detail.html', {'post':post})
 
 
-class PostDeleteView(View):
+class PostDeleteView(LoginRequiredMixin, View):
     def get(self, request, post_id):
         post = Post.objects.get(pk=post_id)
         if request.user.id == post.user.id:
@@ -24,3 +26,22 @@ class PostDeleteView(View):
         else:
             messages.error(request, 'you cant delete this post', 'danger')
         return redirect('home:home')
+
+
+class PostUpdateView(LoginRequiredMixin, View):
+    form_class = PostUpdateForm
+
+    def dispatch(self, request, *args, **kwargs):
+        post = Post.objects.get(pk=kwargs['post_id'])
+        if not post.user.id == request.user.id:
+            messages.error(request, 'you cant update this post', 'danger')
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        form = self.form_class(instance=post)
+        return render(request, 'home/update.html', {'form':form})
+
+    def post(self, request, post_id):
+        pass
+
