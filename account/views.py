205135f763b2d77804feeb8d8672601e76a0757a
unknown <nimamollayi@gmail.com>
2024-04-25 13:50:59 +0330

    handle 404 error in project

--- account/views.py
+++ account/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
@@ -67,6 +67,6 @@ class UserLogoutView(LoginRequiredMixin, View):
 
 class UserProfileView(LoginRequiredMixin, View):
     def get(self, request, user_id):
-        user = User.objects.get(id=user_id)
+        user = get_object_or_404(User, id=user_id)
         posts = Post.objects.filter(user=user)
         return render(request, 'account/profile.html', {'user':user, 'posts':posts})
