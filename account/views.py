162100d6666b276dbcc589eb66bb2faeea0890cc
unknown <nimamollayi@gmail.com>
2024-04-27 19:03:23 +0330

    add next option

--- account/views.py
+++ account/views.py
@@ -38,6 +38,9 @@ class UserLoginView(View):
     form_class = UserLoginForm
     template_name = 'account/login.html'
 
+    def setup(self, request, *args, **kwargs):
+        self.next = request.GET.get('next')
+        return super().setup(request, *args, **kwargs)
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('home:home')
@@ -55,6 +58,8 @@ class UserLoginView(View):
             if user is not None:
                 login(request, user)
                 messages.success(request, 'user loged in succesfully', 'success')
+                if self.next:
+                    return redirect(self.next)
                 return redirect('home:home')
             messages.error(request, 'username or password is wrong', 'warning')
 
