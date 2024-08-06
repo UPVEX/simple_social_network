09be1840389d5c54f8ba9d52cb66126a7e5d5650
nima mollayi <101062448+UPVEX@users.noreply.github.com>
2024-08-06 19:18:21 +0330

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     #### Apps ####
     'home.apps.HomeConfig',
     'account.apps.AccountConfig',
